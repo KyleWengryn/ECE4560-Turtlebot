acbf84ee7d8cf9547a3b987d809043a0fd9ff05f
KyleWengrynBMOC <kylejw@buildingmoc.com>
2022-12-05 13:13:22 -0500

    update

--- laser_scan.py
+++ laser_scan.py
@@ -2,7 +2,7 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print len(msg.ranges)
+    print(msg)
 
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('/kobuki/laser/scan', LaserScan, callback)
