86bae68b90d2f1c2bdd3fc7e118c633c7ed8bd73
KyleWengrynBMOC <kylejw@buildingmoc.com>
2022-12-05 18:46:55 -0500

    update

--- follow_object.py
+++ follow_object.py
@@ -85,7 +85,7 @@ def show_color_highlight(img):
     
     distance = NORM_IMAGE[cY, cX]
 
-    linear = (distance - 0.20) * 0.3
+    linear = (distance - 0.15) * 0.3
     if linear > 0.20:
         linear = 0.20
     if linear < -0.20:
