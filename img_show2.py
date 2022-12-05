0c3d03a9bd1ea5fc3c65bd54868740f1f3daa358
KyleWengrynBMOC <kylejw@buildingmoc.com>
2022-12-05 16:10:10 -0500

    update

--- img_show2.py
+++ img_show2.py
@@ -91,7 +91,7 @@ def image_callback2(img_msg):
         cv_image = bridge.imgmsg_to_cv2(img_msg, "32FC1")
         # Convert the depth image to a Numpy array since most cv2 functions
         # require Numpy arrays.
-        cv_image_array = np.array(cv_image, dtype = np.dtype('f8'))
+        cv_image_array = np.array(cv_image)
         # Normalize the depth image to fall between 0 (black) and 1 (white)
         # http://docs.ros.org/electric/api/rosbag_video/html/bag__to__video_8cpp_source.html lines 95-125
         cv_image_norm = cv2.normalize(cv_image_array, cv_image_array, 0, 1, cv2.NORM_MINMAX)
